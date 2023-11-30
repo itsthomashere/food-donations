def811f99cc1260a75f5deeeafc150d47ad1b701
itsthomashere <thomasadam.leigh@gmail.com>
2023-11-30 17:54:40 +0200

    Adding function checking if an item exists in donation_log with today's date

--- scanner.py
+++ scanner.py
@@ -1,7 +1,24 @@
 import streamlit as st
+from sqlalchemy import create_engine, text
+
+
+def in_donations_table(product_code, date):
+    """Checks to see if an item already exists in the dataset"""
+    query = text(f"""
+    SELECT COUNT(*)
+    FROM donation_log
+    WHERE product_code = {product_code} AND date_received = {date};
+    """)
+    conn = st.connection("digitalocean", type="sql")
+    with conn.session as s:
+        s.execute(query)
+        s.commit()
+    return bool(query) if len(query) <=1 else query
+
 
 def receive_barcodes():
     user_input = st.text_input("Enter a barcode")
     if user_input:
-        st.write(user_input)
+        result = in_donations_table(user_input, '2023-11-30')
+        st.write(result)
 
