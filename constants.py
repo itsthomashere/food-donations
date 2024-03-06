3e1298617c86f18c9b2b6858fe70a45f68f6241e
Thomas Leigh <thomas@fluently.tech>
2024-03-06 15:45:46 +0200

    Commenting out donation_log

--- constants.py
+++ constants.py
@@ -1,15 +1,15 @@
-DONATION_LOG_TABLE = """
-CREATE TABLE IF NOT EXISTS donation_log (
-date_received DATE,
-product_code VARCHAR(13),
-product_name VARCHAR(255),
-category VARCHAR(255),
-price NUMERIC(10, 2),
-weight NUMERIC(10, 2),
-quantity INT,
-total_price NUMERIC(10, 2),
-total_weight NUMERIC(10, 2));
-"""
+# DONATION_LOG_TABLE = """
+# CREATE TABLE IF NOT EXISTS donation_log (
+# date_received DATE,
+# product_code VARCHAR(13),
+# product_name VARCHAR(255),
+# category VARCHAR(255),
+# price NUMERIC(10, 2),
+# weight NUMERIC(10, 2),
+# quantity INT,
+# total_price NUMERIC(10, 2),
+# total_weight NUMERIC(10, 2));
+# """
 
 DONATION_HISTORY_TABLE = """
 CREATE TABLE IF NOT EXISTS donation_history (
