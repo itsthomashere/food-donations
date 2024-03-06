36fb722b2362202f91e5b2976f6f5fb658d2ae15
Thomas Leigh <leigh@fluently.local>
2024-03-06 09:53:38 +0200

    Adding dataclasses for the table data

---
+++ models.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class MissingBarcode:
+    product_code: str
+
+@dataclass
+class DonatedFoodItem:
+    date_received: str
+    product_code: str
+    product_name: str
+    category: str
+    price: float
+    weight: float
+    quantity: int
+    total_price: float
+    total_weight: float
+
+@dataclass
+class DatasetItem:
+    product_code: str
+    product_name: str
+    category: str
+    price: float
+    weight: float
