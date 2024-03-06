0b5e0cadbc0d66aa7cae25672220f4693beba9de
Thomas Leigh <leigh@fluently.local>
2024-03-06 10:01:43 +0200

    Adding donation history table, beginning migration to better name

--- constants.py
+++ constants.py
@@ -11,6 +11,19 @@ total_price NUMERIC(10, 2),
 total_weight NUMERIC(10, 2));
 """
 
+DONATION_HISTORY_TABLE = """
+CREATE TABLE IF NOT EXISTS donation_history (
+date_received DATE,
+product_code VARCHAR(13),
+product_name VARCHAR(255),
+category VARCHAR(255),
+price NUMERIC(10, 2),
+weight NUMERIC(10, 2),
+quantity INT,
+total_price NUMERIC(10, 2),
+total_weight NUMERIC(10, 2));
+"""
+
 MISSING_BARCODES_TABLE = """
 CREATE TABLE IF NOT EXISTS missing_barcodes (
 product_code VARCHAR(13) PRIMARY KEY);
