acc9ef52e94679aaa02e0a675ab79f9af4e1e57b
Thomas Leigh <thomas@fluently.tech>
2024-03-12 17:03:31 +0200

    Swapping out database name to get connection to work

--- db.py
+++ db.py
@@ -2,11 +2,11 @@ import streamlit as st
 from sqlmodel import create_engine, SQLModel
 
 
-username = st.secrets["database"]["username"]
-password = st.secrets["database"]["password"]
-host = st.secrets["database"]["host"]
-port = st.secrets["database"]["port"]
-database_name = st.secrets["database"]["dialect"]
+username = st.secrets["connections.digitalocean"]["username"]
+password = st.secrets["connections.digitalocean"]["password"]
+host = st.secrets["connections.digitalocean"]["host"]
+port = st.secrets["connections.digitalocean"]["port"]
+database_name = st.secrets["connections.digitalocean"]["dialect"]
 
 postgresql_url = f"postgresql://{username}:{password}@{host}:{port}/{database_name}"
 
