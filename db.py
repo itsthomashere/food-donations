976060528a9b2d0e3850c6d136cad5740a566534
Thomas Leigh <thomas@fluently.tech>
2024-03-12 16:39:43 +0200

    Adding st.secrets postgres details. Attempting to make connection.

---
+++ db.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from sqlmodel import create_engine, SQLModel
+
+
+username = st.secrets["database"]["username"]
+password = st.secrets["database"]["password"]
+host = st.secrets["database"]["host"]
+port = st.secrets["database"]["port"]
+database_name = st.secrets["database"]["database_name"]
+
+postgresql_url = f"postgresql://{username}:{password}@{host}:{port}/{database_name}"
+
+engine = create_engine(postgresql_url, echo=False)
+
