9b15785f7eb013178b5e52ceb2b131c9dfbd60fe
Thomas Leigh <thomas@fluently.tech>
2024-03-13 10:48:43 +0200

    Complete overhaul of models.py with SQLAlchemy

--- models.py
+++ models.py
@@ -1,57 +1,33 @@
-from dataclasses import dataclass
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from typing import Optional
+import datetime
 
-from sqlmodel import SQLModel, Field
-from datetime import datetime
+Base = declarative_base()
 
-# Define the model representing your dataset table
-class Dataset(SQLModel, table=True):
+class FoodItem(Base):
     __tablename__ = 'dataset'
-    product_code: Optional[int] = Field(default=None, primary_key=True)
-    product_name: str
-    category: str
-    price: float
-    weight: float
+    product_code = Column(Integer, primary_key=True, index=True, nullable=True)
+    product_name = Column(String, nullable=False)
+    category = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    weight = Column(Float, nullable=False)
 
-class DonatedFoodItem(SQLModel, table=True):
+class DonatedFoodItem(Base):
     __tablename__ = "donation_history"
-    date_received: datetime
-    product_code: Optional[int] = Field(default=None, primary_key=True)
-    product_name: str
-    category: str
-    price: float
-    weight: float
-    quantity: int
-    total_price: float
-    total_weight: float
+    date_received = Column(DateTime, nullable=False)
+    product_code = Column(Integer, primary_key=True, index=True, nullable=True)
+    product_name = Column(String, nullable=False)
+    category = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    weight = Column(Float, nullable=False)
+    quantity = Column(Integer, nullable=False)
+    total_price = Column(Float, nullable=False)
+    total_weight = Column(Float, nullable=False)
 
-class MissingBarcode(SQLModel, table=True):
+class MissingItem(Base):
     __tablename__ = 'barcode_queue'
-    date_added: datetime
-    product_code: Optional[int] = Field(default=None, primary_key=True)
-    status: str
-
-# @dataclass
-# class MissingBarcode:
-#     product_code: str
-
-# @dataclass
-# class DatasetItem:
-#     product_code: int
-#     product_name: str
-#     category: str
-#     price: float
-#     weight: float
-
-# @dataclass
-# class DonatedFoodItem:
-#     date_received: str
-#     product_code: int
-#     product_name: str
-#     category: str
-#     price: float
-#     weight: float
-#     quantity: int
-#     total_price: float
-#     total_weight: float
-
+    date_added = Column(DateTime, nullable=False)
+    product_code = Column(Integer, primary_key=True, index=True, nullable=True)
+    status = Column(String, nullable=False)
