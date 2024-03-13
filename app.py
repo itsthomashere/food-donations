3252c96e9beb8bd2ae658d71090ee6921607c2ac
Thomas Leigh <thomas@fluently.tech>
2024-03-13 11:09:19 +0200

    Adding try / except and input parsing routes.

--- app.py
+++ app.py
@@ -13,7 +13,18 @@ def main():
     conn = dbo.get_connection()
     display_page_title("Woolworths Food Donations")
 
-    st.chat_input("Enter a barcode: ")
+    user_input = st.chat_input("Enter a barcode: ")
+
+    if user_input:
+        try:
+            routes = ip.get_routes()
+            ip.parse_input(user_input)
+        except Exception as e:
+            st.error(e)
+
+
+
+    # Validate and parse the input
 
 
 main()
