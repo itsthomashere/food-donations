3224a9f4f85a63e3305d7859a1926302b0108339
Thomas Leigh <thomas@fluently.tech>
2024-03-13 12:13:20 +0200

    Adding segment of code that deals with MissingItems

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from layout import set_page_config_and_hide_defaults, display_page_title
 
 
 def main():
-    set_page_config_and_hide_defaults() # must always be called first
+    set_page_config_and_hide_defaults()  # must always be called first
     conn = dbo.get_connection()
     display_page_title("Woolworths Food Donations")
 
@@ -24,24 +24,47 @@ def main():
 
             if action == "display" and data in routes:
                 dbo.fetch_all_items(conn, routes[data])
-            
+
             elif action == "barcode":
                 barcode, quantity = data
                 st.write(f"Barcode: {barcode}, Quantity: {quantity}")
 
                 # with barcode and quantity in hand, we can now continue to retrieve the item from the database
                 try:
-                    item = conn.query("select * from dataset where product_code = :product_code", ttl=3600, params={"product_code": barcode})
+                    item = conn.query(
+                        "select * from dataset where product_code = :product_code",
+                        ttl=3600,
+                        params={"product_code": barcode},
+                    )
                     if not item.empty:
                         food_item_row = item.iloc[0]
                         food_item = FoodItem(
-                                product_code=food_item_row['product_code'],
-                                product_name=food_item_row['product_name'],
-                                category=food_item_row['category'],
-                                price=food_item_row['price'],
-                                weight=food_item_row['weight']
-                            )
+                            product_code=food_item_row["product_code"],
+                            product_name=food_item_row["product_name"],
+                            category=food_item_row["category"],
+                            price=food_item_row["price"],
+                            weight=food_item_row["weight"],
+                        )
                         st.write(food_item)
+                    else:
+                        st.write("Adding MissingItem to barcode_queue...")
+                        missing_item = MissingItem(
+                            date_added=datetime.now(),
+                            product_code=barcode,
+                            status="pending",
+                        )
+                        with conn.session as session:
+                            session.execute(
+                                """
+                                                INSERT INTO barcode_queue (date_added, product_code, status)
+                                                VALUES (:date_added, :product_code, :status)""",
+                                {
+                                    "date_added": missing_item.date_added,
+                                    "product_code": missing_item.product_code,
+                                    "status": missing_item.status,
+                                },
+                            )
+                            session.commit()
                 except Exception as e:
                     st.error(f"Error retrieving item: {e}")
 
@@ -50,5 +73,6 @@ def main():
 
         except Exception as e:
             st.error(e)
+
 
 main()
