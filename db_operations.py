52ffb41dad259c9c76d498fc623f1498d37a9539
Thomas Leigh <thomas@fluently.tech>
2024-03-13 10:57:43 +0200

    Adding get_connection to db_operations

--- db_operations.py
+++ db_operations.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 from sqlalchemy import create_engine, text
 from sqlalchemy.engine.base import Connection
+
+def get_connection() -> Connection:
+    """Establishes and returns a database connection."""
+    return st.connection("digitalocean", type="sql", autocommit=True)
