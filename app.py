eec12d535ac39b1ebeeeef571e9a549d9f3f5311
Thomas Leigh <thomas@fluently.tech>
2024-03-13 11:22:58 +0200

    Removing main() altogether due to streamlit problems.

--- app.py
+++ app.py
@@ -8,36 +8,29 @@ from models import FoodItem, DonatedFoodItem, MissingItem
 from layout import set_page_config_and_hide_defaults, display_page_title
 
 
-def main():
-    set_page_config_and_hide_defaults() # must always be called first
-    conn = dbo.get_connection()
-    display_page_title("Woolworths Food Donations")
 
-    user_input = st.chat_input("Enter a barcode: ")
-    st.write("Missing item: 6009226866340")
-    st.write("Dataset item: 6009233018428")
+set_page_config_and_hide_defaults() # must always be called first
+conn = dbo.get_connection()
+display_page_title("Woolworths Food Donations")
 
-    if user_input:
-        try:
-            routes = ip.get_routes()
-            action, data = ip.parse_input(user_input)
+user_input = st.chat_input("Enter a barcode: ")
+st.write("Missing item: 6009226866340")
+st.write("Dataset item: 6009233018428")
 
-            if action == "display" and data in routes:
-                dbo.fetch_all_items(conn, routes[data])
-            
-            elif action == "barcode":
-                barcode, quantity = data
-                st.write(f"Barcode: {barcode}, Quantity: {quantity}")
+if user_input:
+    try:
+        routes = ip.get_routes()
+        action, data = ip.parse_input(user_input)
 
-            else:
-                st.write("Invalid input or table not found.")
+        if action == "display" and data in routes:
+            dbo.fetch_all_items(conn, routes[data])
+        
+        elif action == "barcode":
+            barcode, quantity = data
+            st.write(f"Barcode: {barcode}, Quantity: {quantity}")
 
-        except Exception as e:
-            st.error(e)
+        else:
+            st.write("Invalid input or table not found.")
 
-
-
-    # Validate and parse the input
-
-
-main()
+    except Exception as e:
+        st.error(e)
