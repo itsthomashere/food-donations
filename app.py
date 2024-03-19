28b89ccad17982ffc1bf0d6438d37b6c4887fb5c
Thomas Leigh <thomas@fluently.tech>
2024-03-19 14:34:02 +0200

    Add handling for items not in donation history

--- app.py
+++ app.py
@@ -38,6 +38,9 @@ def main():
                         st.write("Item already in donation history.")
                         # update the quantity and total_price/total_weight
                         updated_item = dbo.update_donation_history_item(conn, barcode, quantity)
+                    else:
+                        st.write("Item not in donation history.")
+                        # retrieve the food item from the dataset
 
                     item = dbo.get_food_item_by_product_code(conn, barcode)
                     # item = conn.query(
