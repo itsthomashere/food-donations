e9345cf9e8f82fc4ac360846b1f0824b3cb53b6d
Thomas Leigh <thomas@fluently.tech>
2024-03-19 18:03:24 +0200

    Undoing decision out of ignorance of st.cache_data

--- db_operations.py
+++ db_operations.py
@@ -7,23 +7,20 @@ from sqlalchemy.orm import Session
 import constants as c
 
 
-@st.cache_data
 def get_connection():
     """Establishes and returns a database connection."""
     return st.connection("digitalocean", type="sql", autocommit=True)
 
-@st.cache_data
-def fetch_all_items(_conn, table):
+def fetch_all_items(conn, table):
     """Fetches all items using the specified SQLAlchemy model."""
-    df = _conn.query(f"select * from {table}", ttl=3600)
+    df = conn.query(f"select * from {table}", ttl=3600)
     st.dataframe(df)
 
-@st.cache_data
-def check_if_item_in_donation_history(_conn, product_code, date):
+def check_if_item_in_donation_history(conn, product_code, date):
     """
     Returns True if a given product_code is located in the donation_history table for a specific date.
     """
-    query_result = _conn.query(c.CHECK_IF_ITEM_IN_DONATION_HISTORY, params={"product_code": product_code, "date_received": date})
+    query_result = conn.query(c.CHECK_IF_ITEM_IN_DONATION_HISTORY, params={"product_code": product_code, "date_received": date})
     
     # Assuming query_result is a DataFrame and the SQL query returns a column named 'item_count'
     return not query_result.empty and query_result.iloc[0]['item_count'] > 0
@@ -34,18 +31,16 @@ def get_current_quantity(_conn, product_code):
     return _conn.query(c.GET_CURRENT_QUANTITY, params={"product_code": product_code})
 
 
-@st.cache_data
-def get_food_item_by_product_code(_conn, product_code, date):
+def get_food_item_by_product_code(conn, product_code, date):
     """Returns a food item from the dataset table given a product_code."""
-    return _conn.query(c.FIND_DATASET_ITEM_BY_PRODUCT_CODE, params={"product_code": product_code})
+    return conn.query(c.FIND_DATASET_ITEM_BY_PRODUCT_CODE, params={"product_code": product_code})
 
-@st.cache_data
-def save_donated_item_to_donation_history(_conn, donated_item):
+def save_donated_item_to_donation_history(conn, donated_item):
     """Saves a donated item to the donation_history table."""
     # Ensure that the SQL command is a text query for execution
     sql_command = text(c.DONATION_HISTORY_INSERT_FOOD_ITEM)
 
-    with _conn.session as session:
+    with conn.session as session:
         session.execute(sql_command, 
                         {"date_received": donated_item.date_received,
                          "product_code": int(donated_item.product_code),
@@ -60,12 +55,14 @@ def save_donated_item_to_donation_history(_conn, donated_item):
                         )
         session.commit()
                     
+def update_donation_history_item(conn, product_code, quantity):
+    """Updates the quantity and total_price/total_weight of a given product_code in donation_history."""
+    st.write("Updating donation history item...")
 
-@st.cache_data
-def add_missing_item_to_queue(_conn, missing_item):
+def add_missing_item_to_queue(conn, missing_item):
     """Adds a missing item to the missing_items table."""
     st.write("Missing item added to queue...")
-    with _conn.session as session:
+    with conn.session as session:
         session.execute(text(c.MISSING_ITEM_INSERT_PRODUCT_CODE),
                         {
                             "date_added": missing_item.date_added,
