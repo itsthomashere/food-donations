00da438318d5a2d507dc6482dd1a1edb91e0effb
Thomas Leigh <thomas@fluently.tech>
2024-03-19 14:20:57 +0200

    Populating dbo functions

--- db_operations.py
+++ db_operations.py
@@ -16,18 +16,19 @@ def fetch_all_items(conn, table):
     df = conn.query(f"select * from {table}", ttl=3600)
     st.dataframe(df)
 
-def get_food_item_by_product_code(conn, product_code):
-    """
-    Retrieves a FoodItem from the database based on the given product_code.
-    """
-    retrieved_item = conn.query("select * from dataset where product_code = :product_code", ttl=3600, params={"product_code": product_code})
-    return retrieved_item
-
 def check_if_item_in_donation_history(conn, product_code, date):
     """Returns True if a given product_code is located in donation_history table."""
     item = conn.query(c.CHECK_IF_ITEM_IN_DONATION_HISTORY, params={"product_code": product_code, "date_received": date})
     st.write(item)
     return True if item else False
                     
+def get_food_item_by_product_code(conn, product_code):
+    """
+    Retrieves a FoodItem from the database based on the given product_code.
+    """
+    retrieved_item = conn.query(c.FIND_DATASET_ITEM_BY_PRODUCT_CODE, ttl=3600, params={"product_code": product_code})
+    return retrieved_item
+
 def update_donation_history_item(conn, product_code, quantity):
-    pass
+    """Updates the quantity and total_price/total_weight of a given product_code in donation_history."""
+    item = conn.query(c.DONATION_HISTORY_INSERT_FOOD_ITEM, params={"product_code": product_code, "quantity": quantity})
