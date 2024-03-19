ce8a0da7d87169b9e409df47d98709cbde9ff1eb
Thomas Leigh <thomas@fluently.tech>
2024-03-19 13:42:24 +0200

    Commenting out models import

--- db_operations.py
+++ db_operations.py
@@ -3,7 +3,7 @@ import streamlit as st
 from sqlalchemy import create_engine, text
 from sqlalchemy.engine.base import Connection
 from sqlalchemy.orm import Session
-from models import FoodItem, DonatedFoodItem, MissingItem
+# from models import FoodItem, DonatedFoodItem, MissingItem
 
 
 def get_connection():
@@ -19,5 +19,5 @@ def get_food_item_by_product_code(session, product_code):
     """
     Retrieves a FoodItem from the database based on the given product_code.
     """
-    return session.query(FoodItem).filter(FoodItem.product_code == product_code).first()
+    pass
 
