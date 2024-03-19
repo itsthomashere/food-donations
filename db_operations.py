ee711acf30777ba2d4c76cbe7f0a33f7708804be
Thomas Leigh <thomas@fluently.tech>
2024-03-19 14:36:14 +0200

    Fix query parameter in check_if_item_in_donation_history function

--- db_operations.py
+++ db_operations.py
@@ -18,7 +18,7 @@ def fetch_all_items(conn, table):
 
 def check_if_item_in_donation_history(conn, product_code, date):
     """Returns True if a given product_code is located in donation_history table."""
-    item = conn.query(c.CHECK_IF_ITEM_IN_DONATION_HISTORY, params={"product_code": product_code, "date_received": date}).all()
+    item = conn.query(c.CHECK_IF_ITEM_IN_DONATION_HISTORY, params={"product_code": product_code, "date_received": date})
     st.write(item)
     return True if item else False
                     
