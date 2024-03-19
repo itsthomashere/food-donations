e414d080632b6711e7f290b3b5eceb365927340e
Thomas Leigh <thomas@fluently.tech>
2024-03-19 16:20:51 +0200

    Remove donation log table definition

--- constants.py
+++ constants.py
@@ -1,15 +1,3 @@
-# DONATION_LOG_TABLE = """
-# CREATE TABLE IF NOT EXISTS donation_log (
-# date_received DATE,
-# product_code VARCHAR(13),
-# product_name VARCHAR(255),
-# category VARCHAR(255),
-# price NUMERIC(10, 2),
-# weight NUMERIC(10, 2),
-# quantity INT,
-# total_price NUMERIC(10, 2),
-# total_weight NUMERIC(10, 2));
-# """
 
 DROP_DONATION_HISTORY_TABLE = """
 DROP TABLE IF EXISTS donation_history;
